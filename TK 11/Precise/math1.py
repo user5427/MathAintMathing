1843c213cfa896ba9041c1c9da516aa82a1e1d81
tari9925 <tadas.riksas@mif.stud.vu.lt>
2025-04-17 10:41:41 +0300

    added task 1 precise solver

--- TK 11/Precise/math1.py
+++ TK 11/Precise/math1.py
@@ -1,4 +1,33 @@
+
+
+import math
+
+
 r = 3
 b = 5
 n = 5
 
+half = n // 2
+if n % 2 != 0:
+    half += 1
+    
+probs = 0
+total = r + b
+for i in range (n - half):
+    reds = i + half
+    blues = n - (i + half)
+    
+    if reds> r:
+        break
+    if blues > b:
+        break
+    
+    # prob = (r / total) ** reds * (b / total) ** blues * math.comb(n, reds)
+    # probs += prob
+    
+    prob = math.comb(n, reds) / math.comb(total, n)
+    probs += prob
+    
+    print(f"reds: {reds}, blues: {blues}")
+    
+print(f"Probability: {probs}")
