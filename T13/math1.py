6bd3a4f532191658c52d27840bb6334aec7aebda
tari9925 <tadas.riksas@mif.stud.vu.lt>
2025-05-22 09:32:58 +0300

    added T13 folder

---
+++ T13/math1.py
@@ -0,0 +1,8 @@
+# Prieš mėnesį prezidentą palankiai vertino P% rinkėjų. Pakartotinai apklausus n
+# rinkėjų, m iš jų prezidento veiklą įvertino teigiamai. Patikrinkite hipotezę,
+# kad rinkėjų, palankiai vertinančių prezidentą, dalis nepasikeitė. Pateikite
+# gautąją p-reikšmę. ( P = 30 , n = 446 m = 117 )
+
+P = 30
+n = 446
+m = 117
---
+++ T13/math4.py
@@ -0,0 +1,12 @@
+# Buvo matuojama, kokiu greičiu (km/val) skraido žvirbliai. Padarę prielaidą,
+# kad žvirblių greitis yra pagal normalųjį dėsnį pasiskirstęs atsitiktinis dydis
+# X ~N(a; o²) ir naudodamiesi imties x duomenimis, patikrinkite hipotezę apie
+# vidutinį greitį Ho a=a_{0}, su alternatyva H₁ a < a_{0}. Pateikite gautąją p-reikšmę.
+# a_{0} = 46.5 x = [47.2, 47.2, 45.7, 45.3, 48.7, 48.2, 44.9, 46.8, 45.8, 44.2,
+# 48.8, 45.6, 48.3, 48.5, 46.9, 48.7, 44.6, 45.8]
+
+a0 = 46.5
+x = [47.2, 47.2, 45.7, 45.3, 48.7, 48.2, 44.9, 
+     46.8, 45.8, 44.2, 48.8, 45.6, 48.3, 48.5, 
+     46.9, 48.7, 44.6, 45.8]
+
